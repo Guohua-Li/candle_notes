42144149351c1f2c7a88f3f4b7f2a8c6decef8f0
Guohua-Li <licharles35@gmail.com>
2024-12-25 21:30:40 -0500

    Update tensor_broadcasting.rs

--- src/tensor_broadcasting.rs
+++ src/tensor_broadcasting.rs
@@ -17,14 +17,6 @@ fn main() -> Result<()> {
     let a = Tensor::new(&[[0.0, 10.0, 20.0, 30.0]], &Cpu)?; // [1,4]
     let b = Tensor::new(&[[1.0], [2.0], [3.0]], &Cpu)?;     // [3,1]
     //let c = (a + b)?;
-    let c = a.broadcast_add(&b)?;
+    let _c = a.broadcast_add(&b)?;
     Ok(())
 }
-
-
-
-/*
-https://jakevdp.github.io/PythonDataScienceHandbook/02.05-computation-on-arrays-broadcasting.html
-
-Broadcasting in coding involves performing operations on arrays of different shapes by stretching smaller arrays to match the dimensions of larger arrays without copying the data. This concept is commonly used in numerical computing libraries like NumPy, enabling element-wise operations on arrays with non-identical shapes for more concise and efficient code.
-*/
